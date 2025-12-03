8802ec01cb9e473e7f56892a384ce00ffad48e44
Lihle Magidigidi <lihlethornmagidigidi@gmail.com>
2025-12-03 16:12:18 +0200

    Edited IRestaurantService interface for Restaurant and MenuItem CRUD operations

--- EmployeeOrderingSystem/Services/IRestaurantService.cs
+++ EmployeeOrderingSystem/Services/IRestaurantService.cs
@@ -1,6 +1,22 @@
-﻿namespace EmployeeOrderingSystem.Services
+﻿using EmployeeOrderingSystem.Models;
+
+namespace EmployeeOrderingSystem.Services
 {
+    // Interface defining CRUD operations for Restaurants and their MenuItems
     public interface IRestaurantService
     {
+        // Restaurant operations
+        Task<List<Restaurant>> GetAllRestaurantsAsync();
+        Task<Restaurant?> GetRestaurantByIdAsync(int id);
+        Task CreateRestaurantAsync(Restaurant restaurant);
+        Task UpdateRestaurantAsync(Restaurant restaurant);
+        Task DeleteRestaurantAsync(int id);
+
+        // MenuItem operations
+        Task<List<MenuItem>> GetMenuItemsByRestaurantIdAsync(int restaurantId);
+        Task<MenuItem?> GetMenuItemByIdAsync(int id);
+        Task CreateMenuItemAsync(MenuItem menuItem);
+        Task UpdateMenuItemAsync(MenuItem menuItem);
+        Task DeleteMenuItemAsync(int id);
     }
 }
