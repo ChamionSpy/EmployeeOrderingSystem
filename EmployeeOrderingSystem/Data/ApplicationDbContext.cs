067205e369a6579506bd0f7c274ccbd36eb3dbb4
Lihle Magidigidi <lihlethornmagidigidi@gmail.com>
2025-12-03 15:28:55 +0200

    Added application db context class and set the employee table

--- EmployeeOrderingSystem/Data/ApplicationDbContext.cs
+++ EmployeeOrderingSystem/Data/ApplicationDbContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmployeeOrderingSystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeOrderingSystem.Data
 {
+    // ApplicationDbContext class inheriting from DbContext
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // set Employee table
+        public DbSet<Employee> Employees { get; set; }
     }
 }
